f7821b287adfad667b64d167e8beb41927941883
Felipebs <07felipebs07@gmail.com>
2024-08-25 13:40:36 -0300

    Added to verify .env of Startup

--- dockadb-api/src/main/java/com/felipebs/dockadb_api/Startup.java
+++ dockadb-api/src/main/java/com/felipebs/dockadb_api/Startup.java
@@ -10,12 +10,8 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class Startup {
 
     public static void main(String[] args) {
-        Dotenv dotenv = Dotenv.load();
-
-        System.out.println("Database Name: " + dotenv.get("MONGO_DB_DATABASE"));
-        System.out.println("Username: " + dotenv.get("MONGO_DB_USERNAME"));
-        System.out.println("Password: " + dotenv.get("MONGO_DB_PASSWORD"));
-        System.out.println("Params: " + dotenv.get("MONGO_DB_PARAMS"));
+        Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+        String database = dotenv.get("MONGO_DB_DATABASE", System.getenv("MONGO_DB_DATABASE"));
         SpringApplication.run(Startup.class, args);
     }
 }
