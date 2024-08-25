5c25c3dfa3079085e5a60e69ab6076b5d5286128
Felipebs <07felipebs07@gmail.com>
2024-08-25 12:45:30 -0300

    Alterations in application.properties and was added .github

--- dockadb-api/src/main/java/com/felipebs/dockadb_api/Startup.java
+++ dockadb-api/src/main/java/com/felipebs/dockadb_api/Startup.java
@@ -1,5 +1,6 @@
 package com.felipebs.dockadb_api;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -9,6 +10,12 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class Startup {
 
     public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.load();
+
+        System.out.println("Database Name: " + dotenv.get("MONGO_DB_DATABASE"));
+        System.out.println("Username: " + dotenv.get("MONGO_DB_USERNAME"));
+        System.out.println("Password: " + dotenv.get("MONGO_DB_PASSWORD"));
+        System.out.println("Params: " + dotenv.get("MONGO_DB_PARAMS"));
         SpringApplication.run(Startup.class, args);
     }
 }
